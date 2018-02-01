8aeab7dcf749be5b6df233adb5d35b61b25b09ce
commit <>
2018-02-01 19:37:23 +0900

    BeginVC 세팅 및 LogInVC 추가

--- OnTheWay/OnTheWay/ViewController/BeginViewControllerViewController.swift
+++ OnTheWay/OnTheWay/ViewController/BeginViewControllerViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class ViewController: UIViewController {
+class BeginViewController: UIViewController {
 
     private var remoteConfig: RemoteConfig!
     
@@ -42,6 +42,7 @@ class ViewController: UIViewController {
     }
     
     func displayWelcome() {
+        let color = remoteConfig["splash_background"].stringValue
         let caps = remoteConfig["splash_message_caps"].boolValue
         let message = remoteConfig["splash_message"].stringValue
         
@@ -51,11 +52,40 @@ class ViewController: UIViewController {
                 exit(0)
             }))
             self.present(alert, animated: true, completion: nil)
+        } else {
+            setLogInVC()
         }
+        self.view.backgroundColor = UIColor(hex: color!)
     }
-
-
+    func setLogInVC() {
+        let loginStoryboard = UIStoryboard(name: "LogIn", bundle: nil)
+        let loginVC = loginStoryboard.instantiateViewController(withIdentifier: "LogInVC") as! LogInViewController
+        self.present(loginVC, animated: true, completion: nil)
+    }
 }
+
+extension UIColor {
+    convenience init(hex: String) {
+        let scanner = Scanner(string: hex)
+        
+        scanner.scanLocation = 1
+        
+        var rgbValue: UInt64 = 0
+        
+        scanner.scanHexInt64(&rgbValue)
+        
+        let r = (rgbValue & 0xff0000) >> 16
+        let g = (rgbValue & 0xff00) >> 8
+        let b = rgbValue & 0xff
+        
+        self.init(
+            red: CGFloat(r) / 0xff,
+            green: CGFloat(g) / 0xff,
+            blue: CGFloat(b) / 0xff, alpha: 1
+        )
+    }
+}
+
 
 
 
---
+++ OnTheWay/OnTheWay/ViewController/LogInViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LogInViewController.swift
+//  OnTheWay
+//
+//  Created by lee on 2018. 2. 1..
+//  Copyright © 2018년 smith. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
