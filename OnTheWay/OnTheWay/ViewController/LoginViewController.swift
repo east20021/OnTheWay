7ca9d86a2d2e240d7c55d4b4fba5d5e311c3f96b
commit <>
2018-03-23 16:04:52 +0900

    pod, gitignore 세팅

---
+++ OnTheWay/OnTheWay/ViewController/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  OnTheWay
+//
+//  Created by lee on 2018. 3. 23..
+//  Copyright © 2018년 smith. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
